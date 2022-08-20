ef513dda84972e56d48fdbd223f45eddccd34f70
marcosbelfastdev <53360034+marcosbelfastdev@users.noreply.github.com>
2022-08-19 23:29:09 -0300

    various

--- src/test/java/framework/core/driver/WebDriverConstrainer.java
+++ src/test/java/framework/core/driver/WebDriverConstrainer.java
@@ -6,7 +6,7 @@ public class WebDriverConstrainer {
 
     WebDriver driver;
 
-    public WebDriverConstrainer(WebDriver driver, Options options) {
+    public WebDriverConstrainer(WebDriver driver) {
         this.driver = driver;
     }
 
